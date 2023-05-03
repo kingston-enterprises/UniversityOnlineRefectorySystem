b96f87f67acbe0289b4938822c384f9b42fe176b
kingston-5 <qhawem54@gmail.com>
2023-05-03 16:40:29 +0200

    added: installed tailwindcss

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -21,7 +21,7 @@ if (!Application::isGuest()) {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-	<link rel="stylesheet" href="/css/tailwind.css" />
+	<link rel="stylesheet" href="/css/output.css" />
 	<script src="https://cdn.tailwindcss.com"></script>
 </head>
 
